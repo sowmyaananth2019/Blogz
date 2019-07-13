29408034246b0aebb89fcd86e52fae69a2b8e670
Sowmya Ananth <sowmyaanantha@gmail.com>
2019-07-13 10:21:04 -0500

    started blogs

--- main.py
+++ main.py
@@ -10,22 +10,42 @@ from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:sowmya123@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:sowmya123@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
-
-
-
-
-class Blog(db.Model):
-
+class Blog(db.Model):      
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    body = db.Column(db.String(500))
+    title = db.Column(db.String(140))
+    body = db.Column(db.String(1200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner = owner
 
 
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+
+    def __repr__(self):
+        return self.username
 
     def __init__(self, title, body):
         self.title = title
